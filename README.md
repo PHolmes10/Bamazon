14a4dca1fad4b268507849b0cd30737e27c14cc2
pholmes10 <pholmes10@gmail.com>
2018-12-17 10:16:03 -0600

    added gifs

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Bamazon
+
+In this app, 
+
+
+![](Bamazon_gifs/product_list.gif)
+
+![](Bamazon_gifs/low_quantity.gif)
+
+![](Bamazon_gifs/order_placed.gif)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,72 +12,72 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     readProducts();
     start();
-  });
+});
 
 function start() {
-    connection.query("SELECT * FROM products", function(err, results) {
+    connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
         // console.log(results);
-    inquirer
-    .prompt([
-        {
-        name: "whichID",
-        type: "list",
-          choices: function() {
-            var choiceArray = [];
-            for (var i = 0; i < results.length; i++) {
-              choiceArray.push(results[i].item_id.toString());
-            }
-            return choiceArray;
-          },
-          message: "What is the ID of the product you would like to buy?",
-    },
-    {
-        name: "howMany",
-        type: "input",
-        message: "How many units would you like to buy?"
-    }
- ]).then(function(answer){
-    var chosenItem;
-        for (var i = 0; i < results.length; i++) {
-          if (results[i].item_id.toString() === answer.whichID) {
-            chosenItem = results[i];
-          };
-        };
-        if (chosenItem.stock_quantity < answer.howMany){
-            console.log("Insufficient Quantity!");
-            start();
-        } else {
-            console.log("Order fulfilled!");
-            connection.query(
-                "UPDATE products SET ? WHERE ?",
-                [
-                  {
-                    stock_quantity: (chosenItem.stock_quantity - answer.howMany)
-                  },
-                  {
-                    item_id: chosenItem.item_id
-                  }
-                ],
-                function(error) {
-                  if (error) throw err;
-                  console.log(`Your purchase cost is ${(chosenItem.price)*(answer.howMany)}`)
-                  start();
+        inquirer
+            .prompt([
+                {
+                    name: "whichID",
+                    type: "list",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].item_id.toString());
+                        }
+                        return choiceArray;
+                    },
+                    message: "What is the ID of the product you would like to buy?",
+                },
+                {
+                    name: "howMany",
+                    type: "input",
+                    message: "How many units would you like to buy?"
                 }
-              );
-        };
- });
-})
+            ]).then(function (answer) {
+                var chosenItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].item_id.toString() === answer.whichID) {
+                        chosenItem = results[i];
+                    };
+                };
+                if (chosenItem.stock_quantity < answer.howMany) {
+                    console.log("Insufficient Quantity!");
+                    start();
+                } else {
+                    console.log("Order fulfilled!");
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quantity: (chosenItem.stock_quantity - answer.howMany)
+                            },
+                            {
+                                item_id: chosenItem.item_id
+                            }
+                        ],
+                        function (error) {
+                            if (error) throw err;
+                            console.log(`Your purchase cost is ${(chosenItem.price) * (answer.howMany)}`)
+                            start();
+                        }
+                    );
+                };
+            });
+    })
 };
 
 function readProducts() {
     console.log("Showing all products...\n");
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.log(res);
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log(res);
     });
-  };
+};
