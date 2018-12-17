729bec11d8e88fe3e54eb8b63fda16fca14620ec
pholmes10 <pholmes10@gmail.com>
2018-12-16 23:39:08 -0600

    added inquirer questions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,13 +14,38 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-
-    readProducts();
+    // readProducts();
+    start();
   });
 
-// function start() {
-//     inquirer.prompt
-// }
+function start() {
+    connection.query("SELECT * FROM products", function(err, results) {
+        if (err) throw err;
+        console.log(results);
+    inquirer
+    .prompt([
+        {
+        name: "whichID",
+        message: "What is the ID of the product you would like to buy?",
+        type: "list",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].item_id.toString());
+            }
+            return choiceArray;
+          },
+    },
+    {
+        name: "howMany",
+        type: "input",
+        message: "How many units would you like to buy?"
+    }
+ ]).then(function(answer){
+     console.log("test");
+ });
+})
+};
 
 function readProducts() {
     console.log("Showing all products...\n");
